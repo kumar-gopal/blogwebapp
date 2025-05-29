c9343955fb045e769312dec7b93f533c1b82ffac
kumar-gopal <kumargopal782001@gmail.com>
2025-05-29 18:15:24 +0530

    user route created

--- backend/server.js
+++ backend/server.js
@@ -5,10 +5,18 @@ import 'dotenv/config';
 const app = express();
 
 const PORT = process.env.PORT || 5000;
+const Name = process.env.NAME || "john";
 
 app.get('/', (req, res) => {
   res.send('Server is running 🚀');
 });
+
+app.get("/users",(req,res)=>{
+  res.json({
+    user : "Gopal kumar",
+    info : "User is fetched successfully"
+  })
+})
 
 app.listen(PORT, () => {
   console.log(`Server is listening on port ${PORT}`);
