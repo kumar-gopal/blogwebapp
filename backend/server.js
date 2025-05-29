6829a20625938aee127b2336827891a1b6c68e2b
kumar-gopal <kumargopal782001@gmail.com>
2025-05-29 18:07:24 +0530

    project initialized

---
+++ backend/server.js
@@ -0,0 +1,15 @@
+// server.js
+import express from 'express';
+import 'dotenv/config';
+
+const app = express();
+
+const PORT = process.env.PORT || 5000;
+
+app.get('/', (req, res) => {
+  res.send('Server is running 🚀');
+});
+
+app.listen(PORT, () => {
+  console.log(`Server is listening on port ${PORT}`);
+});
